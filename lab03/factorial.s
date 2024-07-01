8a271469f56fc39ccb94ab646afc164c27fe8d95
PeterW-XD <peiran_wang@outlook.com>
2024-06-30 23:25:12 -0400

    lab3 exc4

--- lab03/factorial.s
+++ lab03/factorial.s
@@ -1,16 +1,16 @@
 .globl factorial
 
 .data
-n: .word 8
+n: .word 3
 
 .text
 main:
     la t0, n
-    lw a0, 0(t0)
+    lw a0, 0(t0) # a0 = n = 8
     jal ra, factorial
 
-    addi a1, a0, 0
-    addi a0, x0, 1
+    addi a1, a0, 0 # return value in a0
+    addi a0, x0, 1 # print return value
     ecall # Print Result
 
     addi a1, x0, '\n'
@@ -22,3 +22,21 @@ main:
 
 factorial:
     # YOUR CODE HERE
+    addi t0, x0, 1
+    beq a0, t0, exit
+    addi sp, sp, -8
+    sw a0, 0(sp)
+    sw ra, 4(sp)
+    addi a0, a0, -1 # fun(n-1)
+    jal factorial
+    mv t1, a0 # return value
+    lw a0, 0(sp)
+    lw ra, 4(sp)
+    addi sp, sp, 8
+    mul a0, a0, t1
+    jr ra
+    
+exit:
+    addi a0, x0, 1
+    jr ra
+    
